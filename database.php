8a2ab03a1c8e59f6c5f92588526ed0ea35df294c
Philipp Lang <11468252+Pleanog@users.noreply.github.com>
2022-06-01 19:18:13 +0200

    further changes

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
 	$host = ""; 			// host name 
 	$db_username = ""; 		// MySQL username 
-	$db_password = ""; // MySQL password 
+	$db_password = ""; 		// MySQL password 
 	$db_name = "";			// database name 
 
 	$mysqli_connection = mysqli_connect($host, $db_username, $db_password, $db_name)or die("Cannot connect to database."); 
